e6dd4e92e995573f279aace64c0330588bd86561
soshal <111147773+soshal@users.noreply.github.com>
2023-07-06 19:11:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+countEL=doclument.getelementbyid("count-el")
+
+let count=0
+
+function increament(){
+    count=count+1
+    countEL.innerhtml=count
+
+}
+
+function save(){
+
+}
---
+++ practice.js
@@ -0,0 +1,8 @@
+var heading = document.getElementById("heading");
+var box = document.getElementById("box");
+var botton = document.getElementById("botton");  
+
+
+box.addEventListener("click",()=>{
+    console.log("gotcha")
+})
